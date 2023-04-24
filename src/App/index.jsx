db494eedf5b16f954e5beb9d3f67a9f6f1a64e0b
Andres Maneiro <amaneiro7@gmail.com>
2023-04-24 04:15:28 -0400

    se cambio el browserrouter por el hashruuter

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,19 +1,19 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Navbar } from '@/components/Navbar'
 import { Inicio } from '@/Page/Inicio'
 
 function App () {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Navbar />}>
             <Route path='/' element={<Inicio />} />
             <Route path='*' element={<p>Not Found</p>} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
